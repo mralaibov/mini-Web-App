edc9fb9c24909842017975804dc18d5a16580b5d
mralaibov <mr.alaibov@gmail.com>
2022-11-21 18:46:52 +0200

    the current token price project

--- src/features/counter/counterAPI.js
+++ src/features/counter/counterAPI.js
@@ -1,6 +1,13 @@
+import axios from "axios";
+
 // A mock function to mimic making an async request for data
 export function fetchCount(amount = 1) {
   return new Promise((resolve) =>
     setTimeout(() => resolve({ data: amount }), 500)
   );
 }
+
+export const getBitcoin = () => axios
+    .get("https://api.coindesk.com/v1/bpi/currentprice.json")
+    .then((response) => response)
+    .catch(function (error) {console.log(error);})
